64776bd670e16d17a57380bc22e46ec7f1166ace
Shailean Hardy <10149313+Shailean@users.noreply.github.com>
2022-09-15 18:39:59 -0400

    Edit to Task 5

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,26 +1,34 @@
-//Taskmanager class created
+// TASK 6 createTaskHTML = (id, name, dueDate)
+/* <div class="card" id="tasks-not-started">
+    <div class="card-body">
+        <h5 class="card-title">Review pull requests</h5>
+            <p class="card-text">Group meeting on Jul 17 at 4:00pm EST to approve and merge code</p>
+            <p>ASSIGNED TO: All <br> DUE DATE: Jul 17, 2022</p>
+            <span class="badge badge-danger">Not Started</span>
+</div> */
+
+
+//TaskManager class created
 class TaskManager { 
     constructor(currentId=0) {
-        this.tasks =[];
+        this.tasks = [];
         this.currentId = currentId;
     }
 
 //addTask method created
-    addTask(taskName, description, assignedTo, dueDate){
+    addTask(name, description, dueDate, assignedTo){
         const task = {
             id: this.currentId++, //increments currentId
-            name: taskName,
-            assignedTo: assignedTo,
-            description: description,
-            dueDate: dueDate,
-            status: 'TODO'
+            name,
+            description,
+            dueDate,
+            assignedTo,
+            status: 'Not Started'
             };
         this.tasks.push(task); //pushing task
-        }
+    } 
+    render(){
+        
+        //call the createTaskHtml function
     }
-    
-
-
-
-
-
+}
