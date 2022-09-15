ab75f601f787fce78238ea7ad6880bc9e47d8a96
Shailean Hardy <10149313+Shailean@users.noreply.github.com>
2022-09-15 18:47:09 -0400

    Edits to Task 5

--- js/index.js
+++ js/index.js
@@ -21,36 +21,36 @@ const dueDate = newTaskDueDateInput.value;
 const assignedTo = newTaskAssignedToInput.value;
 
 // Validate form input (FUNCTION NOT CURRENTLY WORKING)
-function validFormFieldInput() {
-  // Validate task name input
-  if (name == "") {
-    alert("Please enter a task name"); 
-    return false;
-  } else {
-      console.log("Name:  " + name);
-  }
-  // Validate task description input
-  if (description == "") {
-    alert("Please enter a task description"); 
-    return false;
-  } else {
-      console.log("Description:  " + description);
-  }
-  // Validate task due date input
-  if (dueDate == "") {
-    alert("Please enter a task due date"); 
-    return false;
-  } else {
-      console.log("Due Date:  " + dueDate);
-  }
-  // Validate task assigned to input
-  if (assignedTo == "") {
-    alert("Please assign the task"); 
-    return false;
-  } else {
-      console.log("Assigned to:  " + assignedTo);
-  }
-};//end bracket for form input validation
+  function validFormFieldInput() {
+    // Validate task name input
+    if (name == "") {
+      alert("Please enter a task name"); 
+      return false;
+    } else {
+        console.log("Name:  " + name);
+    }
+    // Validate task description input
+    if (description == "") {
+      alert("Please enter a task description"); 
+      return false;
+    } else {
+        console.log("Description:  " + description);
+    }
+    // Validate task due date input
+    if (dueDate == "") {
+      alert("Please enter a task due date"); 
+      return false;
+    } else {
+        console.log("Due Date:  " + dueDate);
+    }
+    // Validate task assigned to input
+    if (assignedTo == "") {
+      alert("Please assign the task"); 
+      return false;
+    } else {
+        console.log("Assigned to:  " + assignedTo);
+    }
+  };//end bracket for form input validation
 
 // Add the task to the task manager
 taskManager.addTask(name, description, dueDate, assignedTo);
